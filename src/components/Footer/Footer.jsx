c88e3898a49ae954dbdee9d228d829096db38d6f
Tech Madi <zolasmandela4@gmail.com>
2022-03-26 12:48:37 +0300

    Changing the CSS

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,7 @@ const Footer = () => {
   return (
     <section className="footer">
       <p className="footer__text">
-        &copy; 2022. All rights reserved. Dennis, Prudence, Ivasha, Otwoma,
-        Mandy.
+        &copy; 2022. All rights reserved. Team Suzwe
       </p>
     </section>
   );
