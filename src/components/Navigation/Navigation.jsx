24766ba8d5486e8c835ef5a7d209292d542cbb49
otwoma <otwomadennis365@gmail.com>
2022-04-05 08:53:00 +0300

    updates and changes, implemented 'drop-down' thingy

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -15,21 +15,45 @@ const Navigation = () => {
             <a href="/" className="nav__item--link">
               fertilizers
             </a>
+            <div className="nav__popover">
+              <a href="/" className="nav__popover--link">Fertilizer 1</a>
+              <a href="/" className="nav__popover--link">Fertilizer 2</a>
+              <a href="/" className="nav__popover--link">Fertilizer 3</a>
+              <a href="/" className="nav__popover--link">Fertilizer 4</a>
+            </div>
           </li>
           <li className="nav__item">
             <a href="/" className="nav__item--link">
               pesticides
             </a>
+            <div className="nav__popover">
+              <a href="/" className="nav__popover--link">Pesticide 1</a>
+              <a href="/" className="nav__popover--link">Pesticide 2</a>
+              <a href="/" className="nav__popover--link">Pesticide 3</a>
+              <a href="/" className="nav__popover--link">Pesticide 4</a>
+            </div>
           </li>
           <li className="nav__item">
             <a href="/" className="nav__item--link">
               fungicides
             </a>
+            <div className="nav__popover">
+              <a href="/" className="nav__popover--link">Fungicide 1</a>
+              <a href="/" className="nav__popover--link">Fungicide 2</a>
+              <a href="/" className="nav__popover--link">Fungicide 3</a>
+              <a href="/" className="nav__popover--link">Fungicide 4</a>
+            </div>
           </li>
           <li className="nav__item">
             <a href="/" className="nav__item--link">
               initiative
             </a>
+            <div className="nav__popover">
+              <a href="/" className="nav__popover--link">Initiative 1</a>
+              <a href="/" className="nav__popover--link">Initiative 2</a>
+              <a href="/" className="nav__popover--link">Initiative 3</a>
+              <a href="/" className="nav__popover--link">Initiative 4</a>
+            </div>
           </li>
         </ol>
       </nav>
