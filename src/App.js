f36e38929321f208e63caf4aedc16614a325c8e0
otwoma <otwomadennis365@gmail.com>
2022-04-08 08:56:57 +0300

    added load animation to the page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 // import "./style.css"
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Hero from "./components/Hero/Hero";
 import Goals from './components/Goals/Goals'
@@ -11,17 +11,38 @@ import NextSteps from "./components/NextSteps/NextSteps";
 
 import CommunityInitiativeFooter from "./Pages/CommunityInitiative/CommunityInitiativeFooter";
 
+import HashLoader from "react-spinners/HashLoader";
+
 function App() {
+  const color = '#0d2818';
+  const [loading, setLoading ] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 6000)
+  }, [])
   return (
     <div className="App">
-      <Navigation />
-      <Hero />
-      <Goals />
-      <RecommendationsAndGoals />
-      <Educative />
-      <Initiatives />
-      <NextSteps />
-      <CommunityInitiativeFooter />
+      {
+        loading ? 
+        <div className="loader">
+          <h1 className="loader__heading">Clima</h1>
+          <HashLoader color={color} loading={loading}  size={150} />
+          <p className="loader__motto">Let's restore nature back to how it was</p>
+        </div> :
+        <>
+        <Navigation />
+        <Hero />
+        <Goals />
+        <RecommendationsAndGoals />
+        <Educative />
+        <Initiatives />
+        <NextSteps />
+        <CommunityInitiativeFooter />
+        </>
+      }
+
     </div>
   );
 }
